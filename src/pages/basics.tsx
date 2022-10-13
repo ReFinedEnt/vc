7d697ef0edd9c463b3a94a0243ad429f3287ad53
EsyWin <dev@krk.finance>
2022-10-13 08:22:19 +0200

    added compress.sh & compressed /src/assets/img/*

--- src/pages/basics.tsx
+++ src/pages/basics.tsx
@@ -1,17 +1,9 @@
-import type { NextPage } from "next";
-import Head from "next/head";
-import { BasicsView } from "../views";
+import type { NextPage } from 'next';
+import Head from 'next/head';
+import { BasicsView } from '../views';
 
 const Basics: NextPage = (props) => {
-  return (
-    <div>
-      <Head>
-        <title>krk.finance | IT Wizardzy NFT DAO 🪄 !</title>
-        <meta name="description" content="krk.finance | solana web3 basics" />
-      </Head>
-      <BasicsView />
-    </div>
-  );
+  return <BasicsView />;
 };
 
 export default Basics;

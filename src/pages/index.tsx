1ecbf67336280ccc55653447c470dcc5a3fbfabb
€$¥ <dev@krk.finance>
2023-01-25 18:43:47 +0100

    fixed a problem with index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,25 +3,26 @@ import Head from "next/head";
 import { HomeView } from "../views";
 
 const Home: NextPage = (props) => {
-  return;
-  <>
-    <Head>
-      <title>krk.finance</title>
-      <meta
-        property="og:title"
-        content="🏗️ chain-agnostic crowdsourced dev guild 👷"
-      />
-      <meta property="og:site_name" content="krk.finance" />
-      <meta property="og:url" content="https://krk.finance/" />
-      <meta property="og:description" content="We learn & buidl stuff." />
-      <meta property="og:type" content="website" />
-      <meta
-        property="og:image"
-        content="https://raw.githubusercontent.com/EsyWin/cdn/main/krk.finance/assets/img/krk_embed_2.png"
-      />
-    </Head>
-    <HomeView />
-  </>;
+  return (
+    <>
+      <Head>
+        <title>krk.finance</title>
+        <meta
+          property="og:title"
+          content="🏗️ chain-agnostic crowdsourced dev guild 👷"
+        />
+        <meta property="og:site_name" content="krk.finance" />
+        <meta property="og:url" content="https://krk.finance/" />
+        <meta property="og:description" content="We learn & buidl stuff." />
+        <meta property="og:type" content="website" />
+        <meta
+          property="og:image"
+          content="https://raw.githubusercontent.com/EsyWin/cdn/main/krk.finance/assets/img/krk_embed_2.png"
+        />
+      </Head>
+      <HomeView />
+    </>
+  );
 };
 
 export default Home;

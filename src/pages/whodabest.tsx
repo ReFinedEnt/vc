0f3ffb8fa89ad5e800a10dc3d88a99a4ec54d7b9
ez-krk <dev@krk.finance>
2023-06-20 22:18:56 +0200

    added wallet connection for pages/whodabest

--- src/pages/whodabest.tsx
+++ src/pages/whodabest.tsx
@@ -2,8 +2,11 @@ import Head from 'next/head';
 import WhoDaBestView from 'views/whodabest';
 import type { NextPage } from 'next';
 import Table from 'components/table/table.component';
+import { useWallet } from '@solana/wallet-adapter-react';
+import Wallet from 'components/wallet/wallet.component';
 
 const WhoDaBest: NextPage = () => {
+  const { publicKey } = useWallet();
   return (
     <>
       <Head>
@@ -18,7 +21,13 @@ const WhoDaBest: NextPage = () => {
           content="https://raw.githubusercontent.com/EsyWin/cdn/main/krk.finance/assets/img/krk_embed_2.png"
         /> */}
       </Head>
-      <WhoDaBestView />
+      {!publicKey ? (
+        <div className="max-w-[100vw] hero mx-auto bg-black min-h-screen flex justify-center items-center -mt-[67.5px]">
+          <Wallet />
+        </div>
+      ) : (
+        <WhoDaBestView />
+      )}
     </>
   );
 };

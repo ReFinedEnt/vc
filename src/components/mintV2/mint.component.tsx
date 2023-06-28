425a9d75d96df10bf4bbea414b472a030eadba96
ez-krk <dev@krk.finance>
2023-06-28 17:38:57 +0200

    added supply, minted and progress on mint page

--- src/components/mintV2/mint.component.tsx
+++ src/components/mintV2/mint.component.tsx
@@ -11,7 +11,7 @@ import {
   createSignerFromWalletAdapter,
   walletAdapterIdentity,
 } from '@metaplex-foundation/umi-signer-wallet-adapters';
-import { mplCandyMachine } from '@metaplex-foundation/mpl-candy-machine';
+import { CandyMachine, mplCandyMachine } from '@metaplex-foundation/mpl-candy-machine';
 import { TokenStandard } from '@metaplex-foundation/mpl-token-metadata';
 import { fetchCandyMachine, fetchCandyGuard, mintV2 } from '@metaplex-foundation/mpl-candy-machine';
 import {
@@ -37,7 +37,7 @@ import type { FC } from 'react';
 
 const MintButton = styled(Button)`
   width: 100%;
-  height: 60px;
+  height: 42px;
   margin-top: 10px;
   margin-bottom: 5px;
   background: linear-gradient(180deg, #604ae5 0%, #813eee 100%);
@@ -63,51 +63,20 @@ const MintV2: FC = () => {
   const [mintState, setMintState] = useState(false);
 
   const [nft, setNft] = useState(null);
-  const [mintGroup, setMintGroup] = useState<string>('Public');
+
+  const [cm, setCm] = useState<CandyMachine | null>(null);
 
   useEffect(() => {
-    if (nfts) {
-      console.log('nfts : ', nfts);
-    }
-    setLoading(false);
-  }, [nfts]);
-
-  const onClickOG = async () => {
-    try {
+    const fetchCM = async () => {
       const candyMachine = await fetchCandyMachine(umi, candyMachinePublicKey);
-      const candyGuard = await fetchCandyGuard(umi, candyMachine.mintAuthority);
-      const nftOwner = createSignerFromWalletAdapter(wallet).publicKey;
-      const nftMint = generateSigner(umi);
-      const tx = await transactionBuilder()
-        .add(setComputeUnitLimit(umi, { units: 800_000 }))
-        .add(createMintWithAssociatedToken(umi, { mint: nftMint, owner: nftOwner }))
-        .add(
-          mintV2(umi, {
-            candyMachine: candyMachine.publicKey,
-            candyGuard: candyGuard.publicKey,
-            nftMint,
-            collectionMint: publicKey('H63DB66TdhzQCZ34QAEPgzayqhPLMudqrF6aQtk4kG7s'),
-            collectionUpdateAuthority: candyGuard.authority,
-            // tokenStandard: candyMachine.tokenStandard,
-            group: some('OGs'),
-            mintArgs: {
-              mintArgs: {
-                nftBurn: some({
-                  requiredCollection: publicKey('5W4F9DFeGfCDLTdaGRvVKdnBcL27aYWdGxJQVrpo2NrS'),
-                  // mint: publicKey(nfts[0]),
-                  // tokenStandard: TokenStandard.NonFungible,
-                }),
-              },
-            },
-          }),
-        )
-        .sendAndConfirm(umi);
-    } catch (error) {
-      console.log(error);
-    }
-  };
+      return candyMachine;
+    };
+    fetchCM()
+      .then((res) => setCm(res))
+      .finally(() => setLoading(false));
+  }, [candyMachinePublicKey, umi]);
 
-  const onClickPublic = async () => {
+  const onClick = async () => {
     try {
       const candyMachine = await fetchCandyMachine(umi, candyMachinePublicKey);
       const candyGuard = await fetchCandyGuard(umi, candyMachine.mintAuthority);
@@ -138,14 +107,6 @@ const MintV2: FC = () => {
     }
   };
 
-  useEffect(() => {
-    if (wallet.publicKey) {
-      if (holder == HolderEnum.Yay) {
-        setMintGroup('OGs');
-      }
-    }
-  }, [wallet.publicKey, holder]);
-
   return (
     <div className="hero h-screen w-screen md:w-auto mx-auto p-4 flex flex-col justify-center items-center">
       {loading ? (
@@ -161,17 +122,19 @@ const MintV2: FC = () => {
                 objectFit="contain"
               /> */}
             </figure>
-            <div className="card-body flex flex-col justify-center items-center">
+            <div className="card-body flex flex-col justify-center items-center w-96">
               <div className="z-[999] w-full h-full bg-base-100 rounded-xl flex flex-col justify-center items-center">
                 <div className="w-full flex flex-col justify-center items-center">
-                  <h2 className="text-xl text-center font-bold mb-2 text-transparent bg-clip-text bg-gradient-to-tr from-[#9945FF] to-[#14F195]">
-                    Vulture City Founder Token
-                  </h2>
-                  {/* <ul>
-                      <li>Supply : {candyMachine.itemsLoaded}</li>
-                      <li>Minted : {candyMachine.itemsMinted.toString()}</li>
-                      <li>Remaining : {candyMachine.itemsRemaining.toString()}</li>
-                    </ul> */}
+                  <h2 className="text-xl text-center font-bold mb-2">Vulture City Founder Token</h2>
+                  <ul className="mb-2">
+                    <li>Supply : {cm.itemsLoaded}</li>
+                    <li>Minted : {cm.itemsRedeemed.toString()}</li>
+                  </ul>
+                  <progress
+                    className="progress progress-warning w-62 my-4"
+                    value={parseInt(cm.itemsRedeemed.toString())}
+                    max={cm.itemsLoaded}
+                  ></progress>
                 </div>
               </div>
 
@@ -182,7 +145,7 @@ const MintV2: FC = () => {
                   </span>
                 </MintButton>
               ) : ( */}
-              <MintButton onClick={onClickPublic} disabled={mintState}>
+              <MintButton onClick={onClick} disabled={mintState}>
                 <span className="text-white text-xl">
                   {holder == HolderEnum.Yay ? 'OG' : 'PUBLIC'} MINT
                 </span>
